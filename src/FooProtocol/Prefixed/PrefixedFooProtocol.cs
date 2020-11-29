a8dbaa55a9e568ec5bcecb15670b973e672da68c
TeBeCo <tebeco@gmail.com>
2020-11-29 03:03:10 +0100

    Preparing Client + Protocol with their respective Reader/Writer for Prefixed or Delimieted protocol

---
+++ src/FooProtocol/Prefixed/PrefixedFooProtocol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooProtocol.Prefixed
+{
+    public class PrefixedFooProtocol
+    {
+    }
+}
